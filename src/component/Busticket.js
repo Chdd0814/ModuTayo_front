bb9b3eeb76265340676f19e3b2254702f55432eb
Yeongchang Park <hazedayz0223@gmail.com>
2023-10-19 11:41:12 +0900

    메인홈페이지 CSS 수정

--- src/component/Busticket.js
+++ src/component/Busticket.js
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from "react";
 import Ticketinfoform from "./Ticketinfoform";
 import { Typography } from "@mui/material";
 import BusticketForm from "./BusticketForm";
+import './FontCss.css';
 // 0910 pm 10 : 11 
 
 function Busticket({ isLoggedIn }) {
@@ -23,7 +24,7 @@ function Busticket({ isLoggedIn }) {
     return (
 
         <div className = "BusticketForm">
-            <Typography variant="h5">버스표</Typography>
+            <Typography fontFamily='GmarketSansMedium' variant="h5">버스표</Typography>
             <BusticketForm isLoggedIn = {isLoggedIn}/>
         </div>
 
